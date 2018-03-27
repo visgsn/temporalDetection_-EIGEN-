7cb901a0f755c0eec6b9cb2a1c68699bde5f7e9c
gueste <steffen.guentert@gmail.com>
2018-03-27 21:19:58 +0200

    copyAndRename.py: Corrected mistake in script desciption

--- KAIST_preparation/copyAndRename.py
+++ KAIST_preparation/copyAndRename.py
@@ -3,9 +3,9 @@
     been extracted with Pjotr's Matlab Toolbox. (By default only training data from set00 to set05)
     Names are chosen according to the scheme of "train_all20".
     Example for images:
-        T_tmp_set00_V000_100019_<UUID>.png         (T_ stands for "thermal", RGB_ for color images)
+        T_tmp_set00_V000_I00019_<UUID>.png         (T_ stands for "thermal", RGB_ for color images)
     Example for annotations:
-        set00_V000_100019.txt
+        set00_V000_I00019.txt
 '''
 
 from _usefulFunctions import *
