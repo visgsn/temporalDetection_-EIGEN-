af57e464c2978587029a6ee8528450c36d4e8847
gueste <steffen.guentert@gmail.com>
2018-03-29 13:00:40 +0200

    Adapted create_data.sh to my needs (NOT ready to use!)

--- KAIST_preparation/create_data.sh
+++ KAIST_preparation/create_data.sh
@@ -4,11 +4,6 @@
 #   - $HOME/data/VOCdevkit/VOC0712/lmdb/VOC0712_test_lmdb
 # and make soft links at examples/VOC0712/
 
-cd $RefineDet_ROOT
-cur_dir=$(cd $( dirname ${BASH_SOURCE[0]} ) && pwd )
-root_dir=$cur_dir/../..
-cd $root_dir
-
 ##### CONFIGURATION ############################################################
 redo=1
 dataset_name="KAIST"
@@ -18,23 +13,28 @@ min_dim=0
 max_dim=0
 width=0
 height=0
-extra_cmd="--encode-type=jpg --encoded"
+extra_cmd="--encode-type=jpg --encoded"                                         # png?!?
 
 ### *** HOME ***
 RefineDet_ROOT  = "$HOME/code/caffe/RefineDet"
+tmpDetRepo_ROOT = "$HOME/code/temporalDetection_(EIGEN)"
 data_root_dir   = "$HOME/data/KAIST"
-mapfile         = "$root_dir/data/$dataset_name/labelmap_voc.prototxt"          # Has to be adapted!!!
 ### *** WORK ***
 #RefineDet_ROOT  = "$HOME/code/caffe/RefineDet"
-#data_root_dir   = "/net4/merkur/storage/deeplearning/users/gueste/data/VOCdevkit/"
-#mapfile         = "$root_dir/data/$dataset_name/labelmap_voc.prototxt"          # Has to be adapted!!!
+#tmpDetRepo_ROOT = "$HOME/code/temporalDetection_(EIGEN)"
+#data_root_dir   = "/net4/merkur/storage/deeplearning/users/gueste/data/KAIST/"     # CHECK this!!!
+
+mapfile         = "$tmpDetRepo_ROOT/KAIST_preparation/labelmap_KAIST.prototxt"
 ################################################################################
+
+
+cd $RefineDet_ROOT
 
 if [ $redo ]
 then
   extra_cmd="$extra_cmd --redo"
 fi
 for subset in trainval test
-do
-  python $root_dir/scripts/create_annoset.py --anno-type=$anno_type --label-map-file=$mapfile --min-dim=$min_dim --max-dim=$max_dim --resize-width=$width --resize-height=$height --check-label $extra_cmd $data_root_dir $root_dir/data/$dataset_name/$subset.txt $data_root_dir/$dataset_name/$db/$dataset_name"_"$subset"_"$db examples/$dataset_name
+do                                                                                                                                                                                                               #--root_dir    #--list_file                              #--out_dir                                                     #--example_dir
+  python $RefineDet_ROOT/scripts/create_annoset.py --anno-type=$anno_type --label-map-file=$mapfile --min-dim=$min_dim --max-dim=$max_dim --resize-width=$width --resize-height=$height --check-label $extra_cmd $data_root_dir $data_root_dir/ImageSets/Main/$subset.txt $data_root_dir/ImageSets/$db/$dataset_name"_"$subset"_"$db examples/$dataset_name
 done
