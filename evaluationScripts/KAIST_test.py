9b9d96ac4c64013f1e1c61b2d6fa8b6673e53d2e
gueste <steffen.guentert@gmail.com>
2018-04-06 11:48:09 +0200

    Removed coco-related code parts in KAIST_test.py

--- evaluationScripts/KAIST_test.py
+++ evaluationScripts/KAIST_test.py
@@ -9,11 +9,11 @@ import sys
 if __name__ == '__main__':
     ##### CONFIGURATION ################################################################################################
     ### *** HOME ***
-    #GPU_ID = 0                                                                             # ORIGINAL
-    #path_prefix = "{}/train_test_data".format(os.environ['HOME'])
+    GPU_ID = 0
+    path_prefix = "{}/train_test_data".format(os.environ['HOME'])
     ### *** WORK ***
-    GPU_ID = int(sys.argv[1])   #Adapted to use with script "StartIfGPUFree.py". GPU to use for execution
-    path_prefix = "/net4/merkur/storage/deeplearning/users/gueste/TRAINING_test"
+    #GPU_ID = int(sys.argv[1])   #Adapted to use with script "StartIfGPUFree.py". GPU to use for execution
+    #path_prefix = "/net4/merkur/storage/deeplearning/users/gueste/TRAINING_test"
 
     test_set = 'voc_2007_test' # 'voc_2007_test' or 'voc_2012_test' or 'coco_2014_minival' or 'coco_2015_test-dev'
     single_scale = True # True: single scale test;  False: multi scale test
@@ -38,7 +38,7 @@ if __name__ == '__main__':
     prototxt = os.path.join(kaist_path, 'deploy.prototxt')
     models = os.listdir(kaist_path)
 
-    mAP = {}
+    mAP = {}    # mean Average Precision
     for model in models:
         if model.find('caffemodel') == -1:
             continue
@@ -62,25 +62,13 @@ if __name__ == '__main__':
     keys = mAP.keys()
     keys.sort()
     templine = []
-    print("#########################################################################################################")
-    print("#########################################################################################################")
-    if 'voc' in test_set:
-        for key in keys:
-            value = mAP.get(key)
-            print("%d\t%.4f"%(key, value))
-            templine.append("%d\t%.4f\n"%(key, value))
-        with open(kaist_path + 'mAP.txt', 'w+') as f:
-            f.writelines(templine)
-    elif 'coco' in test_set:
-        print("Iter\tAP@0.5:0.95\tAP@0.5\tAP@0.75\tAP@S\tAP@M\tAP@L\tAR@1\tAR@10\tAR@100\tAR@S\tAR@M\tAR@L")
-        templine.append("Iter\tAP@0.5:0.95\tAP@0.5\tAP@0.75\tAP@S\tAP@M\tAP@L\tAR@1\tAR@10\tAR@100\tAR@S\tAR@M\tAR@L\n")
-        for key in keys:
-            value = mAP.get(key) * 100
-            print("%d\t    %.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f"
-                            %(key,value[0],value[1],value[2],value[3],value[4],value[5],value[6],value[7],value[8],value[9],value[10],value[11]))
-            templine.append("%d\t    %.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\n"
-                            %(key,value[0],value[1],value[2],value[3],value[4],value[5],value[6],value[7],value[8],value[9],value[10],value[11]))
-        with open(kaist_path + 'mAP.txt', 'w+') as f:
-            f.writelines(templine)
-    print("#########################################################################################################")
-    print("#########################################################################################################")
+    print("########################################################################")
+    print("########################################################################")
+    for key in keys:
+        value = mAP.get(key)
+        print("%d\t%.4f"%(key, value))
+        templine.append("%d\t%.4f\n"%(key, value))
+    with open(kaist_path + 'mAP.txt', 'w+') as f:
+        f.writelines(templine)
+    print("########################################################################")
+    print("########################################################################")
