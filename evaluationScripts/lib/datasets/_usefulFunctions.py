7a2f2e808b0dd955c6945440cda5e5df75ed01fc
gueste <steffen.guentert@gmail.com>
2018-04-09 21:23:56 +0200

    Added _usefulFunctions to library and adapted kaist_eval.py to my needs.

---
+++ evaluationScripts/lib/datasets/_usefulFunctions.py
@@ -0,0 +1,62 @@
+'''
+    This module implements some useful functions which can be used in other scripts.
+'''
+import os
+import sys
+import re
+import logging
+
+### Configurations ###
+logging.basicConfig(format='%(asctime)s:  %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
+######################
+
+
+def fileParts(fileWithPath):
+    '''
+    This method takes the whole path of a file and returns ['path', 'fileName', 'fileExtension']
+    :param fileWithPath: Complete path with file name and extension.
+    :return: ['path', 'fileName', 'fileExtension'] as strings
+    '''
+    fileWithPath = str(fileWithPath)
+    if len(fileWithPath) != 0:
+        splitFP = os.path.split(fileWithPath)
+
+        path        = splitFP[0]
+        name        = os.path.splitext(splitFP[1])[0]
+        extension   = os.path.splitext(splitFP[1])[1]
+
+        if len(name) != 0:
+            logging.debug(str([path, name, extension]))
+            return [path, name, extension]
+        else:
+            logging.error("ERROR: Empty file name!")
+
+    else:
+        logging.warning("WARNING: Given path is empty!")
+
+
+def dirRecursive(dirToSearch, regexToUse):
+    '''
+        This method takes a path which has to be searched through and a regex which is used to find files inside it
+        that match its pattern. An alphabetically sorted list of all matching files inside dirToSearch is returned.
+        
+        HINT: To find files with a specific extension, e.g. '.py', use a regex like '.*.py$' to search from
+        the end of the filenames.
+        :param dirToSearch: Root directory which has to be searched through
+        :param regexToUse: Filter pattern to use
+        :return: A list of all matching files inside dirToSearch
+    '''
+    filesInFolder = []
+    pattern = re.compile(regexToUse, re.IGNORECASE)
+    for root, dir, fileList in os.walk(dirToSearch):
+        for file in fileList:
+            logging.debug("--------------------------")
+            logging.debug("File:   " + str(file))
+            logging.debug("Match?: " + str(pattern.match(file)))
+            if pattern.match(file):
+                #Reconstruct full file path
+                fileWithPath = os.path.join(root, file)
+                # Add file to List
+                filesInFolder.append(fileWithPath)
+
+    return sorted(filesInFolder, key=str.lower)
