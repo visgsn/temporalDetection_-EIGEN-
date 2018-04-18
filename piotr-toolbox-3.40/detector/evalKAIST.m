957599dc5ebe1e14bcb1846583a3fac45c902b58
gueste <steffen.guentert@gmail.com>
2018-04-18 15:58:07 +0200

    Initial implementation of kaistTest.m and adaption of evalKAIST.m

--- piotr-toolbox-3.40/detector/evalKAIST.m
+++ piotr-toolbox-3.40/detector/evalKAIST.m
@@ -1,3 +1,4 @@
+function [miss,roc,gt,dt] = evalKAIST(outDir, dataDir, images, bbsNmFile)
 % Demo for aggregate channel features object detector on KAIST dataset.
 %
 % See also acfReadme.m
@@ -13,25 +14,37 @@
 %   - detector/acfTest.m
 %   - channels/chnsPyramid.m
 %   - channels/TMagTOri.m (added)
+%
+% INPUTS
+%  outDir    - Directory to save test results (With trailing "/"!)
+%  dataDir   - Directory containing KAIST data (DEFAULT: $KAIST_root/data-kaist/)
+%  images    - Images to use for testing (full path) (matching annotations!)
+%  bbsNmFile - Detections (must be of form [x y w h wt bbType] as .txt file)
+%
+% OUTPUTS
+%  miss      - log-average miss rate computed at reference points
+%  roc       - [nx3] n data points along roc of form [score fp tp]
+%  gt        - [mx5] ground truth results [x y w h match] (see bbGt>evalRes)
+%  dt        - [nx6] detect results [x y w h score match] (see bbGt>evalRes)
+
 
 %% extract training and testing images and ground truth
 cd(fileparts(which('evalKAIST.m')));
-dataDir='../../data-kaist/';                                                % Adapt this! (Give parameter?!?)
 addpath( genpath( '..' ) );
 
-cond = { '-all', '-day', '-night' };
-for c=1:3,                                                                  % COMMENT OUT (WHOLE loop)!!!
-    type=['test' cond{c}]; skip=[];
-    dbInfo2(['kaist-' type]);
-    if(exist([dataDir type '/annotations'],'dir')), continue; end
-    dbExtract2(dataDir, type,1,skip);
-end
+%cond = { '-all', '-day', '-night' };
+%for c=1:3,                                                                 % COMMENT OUT (WHOLE loop)!!!
+%    type=['test' cond{c}]; skip=[];
+%    dbInfo2(['kaist-' type]);
+%    if(exist([dataDir type '/annotations'],'dir')), continue; end
+%    dbExtract2(dataDir, type,1,skip);
+%end
 % Not used with caffe detector:
-type='train-all'; skip=20; 
-dbInfo2( ['kaist-' type] ); type=[ type int2str2(skip,2)];                  % COMMENT OUT (Return values needed?)???
-if(~exist([dataDir type '/annotations'],'dir')),  dbExtract2(dataDir, type,1,skip); end     % COMMENT OUT!!!
+%type='train-all'; skip=20; 
+%dbInfo2( ['kaist-' type] ); type=[ type int2str2(skip,2)];                 % COMMENT OUT (Return values needed?)???
+%if(~exist([dataDir type '/annotations'],'dir')),  dbExtract2(dataDir, type,1,skip); end     % COMMENT OUT!!!
 
-%% set up opts for training detector (see acfTrain)
+%% set up opts for training detector (see acfTrain) (ONLY DUMMY VARIABLES!)
 opts=acfTrain2(); opts.modelDs=[50 20.5]; opts.modelDsPad=[64 32];
 opts.pPyramid.smooth=.5;
 opts.pPyramid.pChns.pColor.smooth=0; opts.nWeak=[32 128 512 2048];
@@ -42,12 +55,14 @@ opts.pJitter=struct('flip',1);
 opts.posGtDir=[dataDir 'train-all' int2str2(skip,2) '/annotations'];
 opts.posImgDir=[dataDir 'train-all' int2str2(skip,2) '/images'];
 
-% opts.name=[ 'models/AcfKAIST-RGB' ];
-% opts.name=[ 'models/AcfKAIST-RGB-T' ];
-% opts.name=[ 'models/AcfKAIST-RGB-T-TM-TO' ];
-opts.name= [ 'models/AcfKAIST-RGB-T-THOG' ];
+%opts.name=[ 'models/AcfKAIST-RGB' ];
+%opts.name=[ 'models/AcfKAIST-RGB-T' ];
+%opts.name=[ 'models/AcfKAIST-RGB-T-TM-TO' ];
+%opts.name=[ 'models/AcfKAIST-RGB-T-THOG' ];
+opts.name=[ outDir 'TestResultsMATLAB/results' ];             % Output path
 
-pLoad={'lbls',{'person'},'ilbls',{'people','person?','cyclist'}};
+% Detection labels to use or ignore
+pLoad={'format',1,'lbls',{'person'},'ilbls',{'people','person?','cyclist'}};    % Adapt format=1 (PascalVoc)?!?
 opts.pLoad = [pLoad 'hRng',[55 inf], 'vType', {'none'} ];
 
 %% To handle thermal channel
@@ -72,27 +87,30 @@ pCustom(3).pFunc = {};
 opts.pPyramid.pChns.pCustom = pCustom;
 
 %% train detector (see acfTrain)
-detector = acfTrain2( opts );                                               % COMMENT OUT!!!
+% detector = acfTrain2( opts );                                               % COMMENT OUT!!!
 
 %% modify detector (see acfModify)
 pModify=struct('cascThr',-1,'cascCal',.025);                                % Doesn't matter if reapply == 0!!!
-detector=acfModify(detector,pModify);                                       % COMMENT OUT!!!
+%detector=acfModify(detector,pModify);                                       % COMMENT OUT!!!
 
-%% run detector on a sample image (see acfDetect)
-cond = '-all';     
-% cond = '-day';
-% cond = '-night';
+%% run detector on a sample image (only example) (see acfDetect)
+cond = '-all';                                         % test subset to use
+%cond = '-day';
+%cond = '-night';
 
 imgDir = [dataDir 'test' cond '/images'];
 gtDir = [dataDir 'test' cond '/annotations'];
 
-imgNms=bbGt('getFiles', {imgDir});                                          % Use f0 and f1 to select RGB-/IR-Images? (Or presort...)
-I=imread(imgNms{102}); tic, bbs=acfDetect(I,detector); toc                  % Give bbs with python! --> remove acfDetect(...)
-figure(3); imshow(I(:,:,1:3)); bbApply('draw',bbs); pause(.1);
+imgNms=bbGt('getFiles', {imgDir}, 2253, 4504);                              % Use f0 and f1 to select RGB-/IR-Images? (Or presort...)
+I=imread(imgNms{102});
+% Reimplement this with own detections!!!
+%tic, bbs=acfDetect(I,detector); toc                                        % Give bbs with python! --> remove acfDetect(...)
+%figure(3); imshow(I(:,:,1:3)); bbApply('draw',bbs); pause(.1);
 
 %% test detector and plot roc (see acfTest)
-[~,~,gt,dt]=acfTest('name',opts.name,'imgDir',imgDir,...
+[miss,roc,gt,dt]= kaistTest('name',opts.name,'imgDir',imgDir,...
   'gtDir',gtDir,'pLoad',[pLoad, 'hRng',[55 inf],...
   'vType',{{'none','partial'}},'xRng',[5 635],'yRng',[5 475]],...
   'pModify',pModify,'reapply',0,'show',11,...
   'lims', [3.1e-3 3.1e1 .2 1],'type', cond, 'clr', 'r', 'lineSt', '-');
+end
