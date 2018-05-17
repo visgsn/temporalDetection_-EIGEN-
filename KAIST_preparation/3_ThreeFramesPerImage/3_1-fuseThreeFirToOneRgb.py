65bb4330cf1b018cf780187f56d6c2bf034eec9d
gueste <steffen.guentert@gmail.com>
2018-05-17 17:42:48 +0200

    Finished basic implementation of 3_1-fuseThreeFirToOneRgb.py

--- KAIST_preparation/3_ThreeFramesPerImage/3_1-fuseThreeFirToOneRgb.py
+++ KAIST_preparation/3_ThreeFramesPerImage/3_1-fuseThreeFirToOneRgb.py
@@ -24,18 +24,18 @@ atWORK          = False  # Choose which config to use: HOME (False) - WORK (True
 
 dataToExtract   = ['train-all-T', 'test-all-T']  # Expects: [<Train_Set>, <Test_Set>] (OutputSubdir <-- Train_Set name)
 imageStepSize   = 5  # Distance between images in channel (R=t, G=t-1*iSS, B=t-2*iSS)
-useThermal      = True  # If False, 'RGB_'-images will be extracted.
 
 kaistDirHOME    = '/home/gueste/data/KAIST/data-kaist'
 kaistDirWORK    = '/net4/merkur/storage/deeplearning/users/gueste/data/KAIST/data-kaist'
 
 mainTestSet     = 'test-all'  # Main test set for comparison (Do not change this for KAIST dataset!)
+useThermal      = True  # If False, 'RGB_'-images will be extracted.
 
 set_V_Pattern   = '(set[0-9]+_V[0-9]+)_'  # Used for comparison of predecessor image names with original image
 logging.basicConfig(format='%(asctime)s:  %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
 ########################################################################################################################
 
-#dataToExtract   = ['test-all-T', 'train-all-T']                                                                         # Wieder entfernen!!!
+dataToExtract   = ['test-all-T', 'train-all-T']                                                                         # Wieder entfernen!!!
 
 kaistDir = kaistDirWORK[:] if atWORK else kaistDirHOME[:]
 assert os.path.exists(kaistDir), \
@@ -132,16 +132,19 @@ for folder in dataToExtract:
         # Assign predecessors behind original image
         imgFiles_out[i] = tmpImgFiles[:]
 
-    img = cv2.imread('/home/gueste/Schreibtisch/T_tmp_set06_V004_I00800_test-all-T (Kopie).png')
-    im_r = img[:, :, 2]
-    im_g = img[:, :, 1]
-    im_b = img[:, :, 0]
-    cv2.imwrite('/home/gueste/Schreibtisch/T_tmp_set06_V004_I00800_test-all-T (neu).png', img)
+    ### Fuse and save images
+    for i in range(0, len(imgFiles_out)):
+        # Load original image (BGR-Format: BGR --> 012)
+        imgOrig = cv2.imread(imgFiles_out[i][0])
+        # Replace channels G (1) and B (0) of original with predecessors R (2) channel
+        for predNr in range(1,3):
+            imgPred = cv2.imread(imgFiles_out[i][predNr])
+            imgOrig[:,:,2-predNr] = imgPred[:,:,2]
+        # Save resulting image in target folder
+        outputFileName = os.path.join( imageDir_out, os.path.split(imgFiles_out[i][0])[1] )
+        cv2.imwrite(outputFileName, imgOrig)
 
-    ### Copy annotations and save fused images
+    ### Copy annotations
     # Annotations
     for singleAnno in annoFiles_out:
         shutil.copy(singleAnno, annoDir_out)
-    # Images
-    for singleImg in imgFiles_out[:][1]:
-        shutil.copy(singleImg, imageDir_out)
