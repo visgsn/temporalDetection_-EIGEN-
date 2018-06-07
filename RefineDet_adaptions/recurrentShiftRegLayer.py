431a51c47abcaa6511eb412315405414d10255b4
Güntert Steffen <gueste@iosb.fraunhofer.de>
2018-06-07 11:26:28 +0200

    Initial implementation of recurrentShiftRegLayer.py

--- RefineDet_adaptions/recurrentShiftRegLayer.py
+++ RefineDet_adaptions/recurrentShiftRegLayer.py
@@ -10,5 +10,22 @@ import caffe
 
 
 ##### CONFIGURATION ####################################################################################################
-
+TRAIN   = 0
+TEST    = 1
 ########################################################################################################################
+
+
+
+class recurrent_shift_reg_layer(caffe.Layer):
+    def setup(self, bottom, top):
+        pass
+
+    def forward(self, bottom, top):
+        pass
+
+    def reshape(self, bottom, top):
+        pass
+
+    def backward(self, bottom, top):
+        pass
+
