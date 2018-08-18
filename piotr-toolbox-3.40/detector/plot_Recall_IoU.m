a8a2be18d1ef3148eb7e9bd18547fb3696fc4f84
gueste <steffen.guentert@gmail.com>
2018-08-18 12:20:07 +0200

    Fixed some bugs in script fo plotting Recall-IoU-Diagram

--- piotr-toolbox-3.40/detector/plot_Recall_IoU.m
+++ piotr-toolbox-3.40/detector/plot_Recall_IoU.m
@@ -6,20 +6,23 @@
 %% Variablen deklarieren
 % Basispfad zur gewuenschten MATLAB-Evaluationsdatei
 % 'MATLAB_comp4_detectionBBs_test_person.txt' einfuegen!!!
-base_path = '/media/gueste/TrekStor/Masterarbeit/Test_Berechnung_RE-IoU/3_Tr9-1/KAIST_3_Tr9-1_3FpI_D4_320x320_iter_1200/detections_for_matlab';
+base_path = ...
+    '/media/gueste/TrekStor/Masterarbeit/Ausgelagerte_Trainings/2_train-all-T_D7/2_Tr16-2_OF_D7_320x320/evalOutput/singleScale/KAIST_2_Tr16-2_OF_D7_320x320_iter_210/detections_for_matlab';
 
 % Konstanten
 plot_von = .5;
 plot_bis = .99;
-teilung  = .01;
+teilung  = .005;
 
 
 %% Neue Pfade Berechnen
 gtDir_NEU = sprintf('%s/data/KAIST/data-kaist/test-all/annotations', getenv('HOME'));
+var_load   = sprintf('%s/%s', base_path, 'MATLAB_comp4_detectionBBs_test_person-all_WsVariables.mat');
 bbsNm_NEU = sprintf('%s/%s', base_path, 'MATLAB_comp4_detectionBBs_test_person.txt');
 
 
 %% Daten Laden
+load(var_load);
 [gt_orig,dt_orig] = bbGt('loadAll',gtDir_NEU,bbsNm_NEU,pLoad);  % gt := gt0, dt := dt0
 
 
@@ -35,11 +38,15 @@ for i = 1:size(iou_thr,2)
 end
 
 
-%%  Plot erzeugen
+%% Plot erzeugen
 f = figure(show);
 %f.Position(3) = 1000; % figure breiter machen
 plot(iou_thr, recall * 100, 'b', 'LineWidth', 2);
 axis([plot_von 1 0 100]); grid on;
-title("Recall and Intersection over Union");
+title("Recall w.r.t. Intersection over Union");
 xlabel("Intersection over Union"); ylabel("Recall [%]");
+set(gca,'FontSize',16)
 
+
+%% Plot speichern
+savefig([base_path '/Recall_IoU' '.fig']);
